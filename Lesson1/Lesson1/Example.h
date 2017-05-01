4c936634a9d00ea5d258e8b071e6469d8e6c17cc
chumak_mac <chumak.ivan@gmail.com>
2017-05-01 22:24:02 +0300

    Ветка для фото

---
+++ Lesson1/Lesson1/Example.h
@@ -0,0 +1,13 @@
+//
+//  Example.h
+//  Lesson1
+//
+//  Created by Ivan Chumak on 01.05.17.
+//  Copyright © 2017 Ivan Chumak. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Example : NSObject
+
+@end
---
+++ Lesson1/Lesson1/Example.m
@@ -0,0 +1,13 @@
+//
+//  Example.m
+//  Lesson1
+//
+//  Created by Ivan Chumak on 01.05.17.
+//  Copyright © 2017 Ivan Chumak. All rights reserved.
+//
+
+#import "Example.h"
+
+@implementation Example
+
+@end
